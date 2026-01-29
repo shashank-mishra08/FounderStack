2ebb5ff1e1c5353c424ad7d3139f0eeec20df89a
shashank kumar <shashankmishra00026@gmail.com>
2026-01-29 12:00:19 +0530

    Feat: Add Vercel configuration for deployment

--- client/lib/api.ts
+++ client/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5001/api', // Can move to env in future
+    baseURL: '/api',
     headers: {
         'Content-Type': 'application/json',
     },
